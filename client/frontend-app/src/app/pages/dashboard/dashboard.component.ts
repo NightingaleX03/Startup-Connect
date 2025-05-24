1824dd7c6d1129cb5bc8b13d511354e3ebfaa8bc
smathew32 <smathew32@myseneca.ca>
2025-05-24 13:05:15 -0400

    added leaderboard to dashboard

--- client/frontend-app/src/app/pages/dashboard/dashboard.component.ts
+++ client/frontend-app/src/app/pages/dashboard/dashboard.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StartupProfileCardComponent } from '../../components/startup-profile-card/startup-profile-card.component';
 import { LocalGroupsComponent } from '../../components/local-groups/local-groups.component';
+import { LeaderboardComponent } from '../../components/leaderboard/leaderboard.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, StartupProfileCardComponent, LocalGroupsComponent],
+  imports: [CommonModule, StartupProfileCardComponent, LocalGroupsComponent, LeaderboardComponent],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
