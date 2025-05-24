dafdb0c2fa04db7c1a2ca8bb53f6486221d4de9e
smathew32 <smathew32@myseneca.ca>
2025-05-23 22:03:36 -0400

    Created Containers for the data to go into

--- client/frontend-app/src/app/pages/dashboard/dashboard.component.ts
+++ client/frontend-app/src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProfileComponent } from '../profile/profile.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ProfileComponent],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
